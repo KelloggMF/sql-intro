aa9fcc4bf4dbc4a362c8457874841279ba832e0b
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-04-06 01:51:05 +0000

    Code for 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,7 @@
 -- Expected result:
 -- 2935
 
-
+SELECT stats.player_id, players.id, players.last_name, players.first_name, SUM(stats.hits)
+FROM players INNER JOIN stats ON stats.player_id = players.id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds"
+GROUP BY stats.player_id;
